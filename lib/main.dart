036c91168c3e6bd40cdd918fba48ced607c2f1e6
Kurogoma4D <highdro0479@gmail.com>
2019-06-20 15:59:49 +0900

    add: provider in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
+import 'package:KRPG/app_model.dart';
+import 'package:KRPG/pages/widgets.dart';
+
 import 'drawer.dart';
 import 'package:flutter/material.dart';
 import 'pages/ideas.dart';
 import 'custom_theme.dart';
 import 'package:flare_flutter/flare_actor.dart';
 import 'package:provider/provider.dart';
+import 'app_model.dart';
 
 void main() => runApp(MyApp());
 
@@ -23,24 +27,41 @@ class MyApp extends StatelessWidget {
               fit: BoxFit.fitHeight,
             ),
           ),
-          Scaffold(
-            appBar: AppBar(
+          ChangeNotifierProvider<AppStateModel>(
+            builder: (_) => AppStateModel(0),
+            child: Scaffold(
+              appBar: AppBar(
+                backgroundColor: Colors.transparent,
+                elevation: 0.0,
+                title: Text("IDEAS"),
+              ),
+              drawer: Drawer(
+                child: DrawerMenu(),
+              ),
+              body: _selectPage(context),
               backgroundColor: Colors.transparent,
-              elevation: 0.0,
-              title: Text("IDEAS"),
-            ),
-            drawer: Drawer(
-              child: DrawerMenu(),
-            ),
-            body: IdeasPage(),
-            backgroundColor: Colors.transparent,
-            floatingActionButton: FloatingActionButton(
-              child: Icon(Icons.add),
-              onPressed: () {},
+              floatingActionButton: FloatingActionButton(
+                child: Icon(Icons.add),
+                onPressed: () {},
+              ),
             ),
           ),
         ],
       ),
     );
+  }
+
+  // ページ増やしたらこれ追加しないといけない
+  Widget _selectPage(BuildContext context) {
+    Widget page;
+    switch (Provider.of<AppStateModel>(context).pageIndex) {
+      case 0:
+        page = IdeasPage();
+        break;
+      case 1:
+        page = WidgetTestPage();
+        break;
+    }
+    return page;
   }
 }
