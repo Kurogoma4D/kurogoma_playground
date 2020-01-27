6b7da420d125a08ede08937e24f9dc27ff12431d
Takafumi Suzuki <highdro0479@gmail.com>
2020-01-27 17:51:20 +0900

    add: widgets base

--- lib/pages/widgets.dart
+++ lib/pages/widgets.dart
@@ -3,17 +3,34 @@ import 'package:flutter/material.dart';
 class WidgetTestPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ListView.separated(
-      itemCount: 5,
-      itemBuilder: _buildListItem,
-      separatorBuilder: (context, index) => Divider(),
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+      child: ListView(
+        children: <Widget>[
+          _neumorphismicButtons(context),
+        ],
+      ),
     );
   }
 
-  Widget _buildListItem(BuildContext context, int index) {
+  Widget _neumorphismicButtons(BuildContext context) {
     return Card(
-      child: Placeholder(
-        fallbackHeight: 100,
+      color: Colors.white,
+      child: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text(
+              'NeumorphismicButtons',
+              style: Theme.of(context).textTheme.title,
+            ),
+            const SizedBox(height: 12),
+            Row(
+              children: <Widget>[],
+            )
+          ],
+        ),
       ),
     );
   }
