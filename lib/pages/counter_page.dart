64c911069f8c3fe21fb103636c02d80b4228f868
Kurogoma4D <highdro0479@gmail.com>
2021-01-09 15:04:20 +0900

    カウンターなおした

--- lib/pages/counter_page.dart
+++ lib/pages/counter_page.dart
@@ -1,22 +1,20 @@
-import 'package:KRPG/states/counter/counter.dart';
 import 'package:KRPG/states/counter/counter_controller.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
+import 'package:flutter_riverpod/all.dart';
 
-class CounterPage extends StatelessWidget {
+class CounterPage extends ConsumerWidget {
   const CounterPage({Key key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, ScopedReader watch) {
+    final counterState = watch(counterControllerProvider.state);
     return Scaffold(
       body: Center(
-        child: Text(
-          '${context.select((Counter c) => c.count)}'
-        ),
+        child: Text('${counterState.count}'),
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
-        onPressed: context.watch<CounterController>().increment,
+        onPressed: () => context.read(counterControllerProvider).increment(),
       ),
     );
   }
